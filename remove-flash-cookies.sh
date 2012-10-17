bbaf2c02e1c8a2a5ac72eb27abaf646a90e8bd48
L.S. Cook <shawnee@hackeress.com>
2012-10-17 06:33:39 -0700

    Update remove-flash-cookies.sh

--- remove-flash-cookies.sh
+++ remove-flash-cookies.sh
@@ -1,4 +1,7 @@
-echo "Tracking cookies not welcome here"
+echo "These are the tracking cookies we're going to get rid of.  How many do you see?"
+ls -aLF $HOME/.macromedia/Flash_Player/#SharedObjects/
+ls -aLF $HOME/.macromedia/Flash_Player/macromedia.com/support/flashplayer/sys/
+echo "Enter sudo password to force removal."
 sudo rm -rf $HOME/.macromedia/Flash_Player/#SharedObjects/*/*
 sudo rm -rf $HOME/.macromedia/Flash_Player/macromedia.com/support/flashplayer/sys/*
-echo "Removed unwanted cookies"
+echo "Removed unwanted cookies. (Re-run the script if you wish to confirm they are actually gone.)"
