fe93f7dddc0a647fefac6a1b9c697ef6f75dcf0d
L.S. Cook <lsc@hackeress.com>
2016-12-20 18:46:00 -0800

    update against adobe's perpetual sneak attacks smh

--- remove-flash-cookies.sh
+++ remove-flash-cookies.sh
@@ -4,6 +4,9 @@ ls -aLF $HOME/.macromedia/Flash_Player/#SharedObjects/
 ls -aLF $HOME/.macromedia/Flash_Player/macromedia.com/support/flashplayer/sys/
 ls -aLF $HOME/.adobe/Flash_Player/AssetCache
 ls -aLF $HOME/.adobe/Flash_Player/NativeCache
+ls -aLF $HOME/.mozilla/firefox/mwad0hks.default/datareporting
+ls -aLF $HOME/.mozilla/firefox/mwad0hks.default/cookies.*
+
 echo "Enter sudo password to force removal."
 sudo rm -rf $HOME/.macromedia/Flash_Player/#SharedObjects/*
 sudo rm -rf $HOME/.macromedia/Flash_Player/macromedia.com/support/flashplayer/sys/*
@@ -11,4 +14,7 @@ sudo rm -rf $HOME/.adobe/Flash_Player/NativeCache
 sudo rm -rf $HOME/.adobe/Flash_Player/AssetCache
 sudo rm -rf $HOME/.cache/mozilla/firefox
 sudo rm -rf $HOME/.cache/*
+sudo rm -rf ~/.mozilla/firefox/mwad0hks.default/datareporting/*
+sudo rm -rf  $HOME/.mozilla/firefox/mwad0hks.default/cookies.*
+
 echo "Removed unwanted cookies. (Re-run the script if you wish to confirm they are actually gone.)"
