f9dc4b7d9e0933e546cf844989fa834325e9ae68
Leona C <leona.cook@intel.com>
2018-07-03 05:25:47 -0700

    wc

--- remove-flash-cookies.sh
+++ remove-flash-cookies.sh
@@ -1,11 +1,14 @@
-echo "These are the tracking cookies we're going to get rid of.  How many do you see?"
+# This needs cleaned up, but it works.  Your Fx default profile may be named something different. 
+
+echo "These are the tracking cookies we're going to get rid of. How many do you see?"
 ls -aLF $HOME/.cache/mozilla/firefox
 ls -aLF $HOME/.macromedia/Flash_Player/#SharedObjects/
 ls -aLF $HOME/.macromedia/Flash_Player/macromedia.com/support/flashplayer/sys/
 ls -aLF $HOME/.adobe/Flash_Player/AssetCache
 ls -aLF $HOME/.adobe/Flash_Player/NativeCache
-ls -aLF $HOME/.mozilla/firefox/mwad0hks.default/datareporting
-ls -aLF $HOME/.mozilla/firefox/mwad0hks.default/cookies.*
+ls -aLF $HOME/.mozilla/firefox/*.*
+ls -aLF $HOME/.mozilla/firefox/*.default/*
+
 
 echo "Enter sudo password to force removal."
 sudo rm -rf $HOME/.macromedia/Flash_Player/#SharedObjects/*
@@ -13,8 +16,12 @@ sudo rm -rf $HOME/.macromedia/Flash_Player/macromedia.com/support/flashplayer/sy
 sudo rm -rf $HOME/.adobe/Flash_Player/NativeCache
 sudo rm -rf $HOME/.adobe/Flash_Player/AssetCache
 sudo rm -rf $HOME/.cache/mozilla/firefox
-sudo rm -rf $HOME/.cache/*
+sudo rm -rf $HOME/.cache/mozilla/firefox/*.default/{cache2,frequencyCap.json,directoryLinks.json,startupCache}
+sudo rm -rf $HOME/.cache/mozilla/firefox/*.default/datareporting/*
 sudo rm -rf ~/.mozilla/firefox/mwad0hks.default/datareporting/*
-sudo rm -rf  $HOME/.mozilla/firefox/mwad0hks.default/cookies.*
+sudo rm -rf  $HOME/.mozilla/firefox/*.default/cookies.*
+sudo rm -rf  $HOME/.mozilla/firefox//kzj780yn.default/cookies.*
+sudo rm -rf ~/.cache/mozilla/*
+sudo rm -rf /.macromedia/Flash_Player/macromedia.com/support/flashplayer/sys/#cdn.livestream.com/settings.sol
 
 echo "Removed unwanted cookies. (Re-run the script if you wish to confirm they are actually gone.)"
